d8b09c16759948641557d7eb11225dfc1af8eb2e
Joel Perry <joelperry@me.com>
2014-10-12 23:13:39 +0100

    Removed woopsie

--- js/main.js
+++ js/main.js
@@ -8,4 +8,4 @@ document.getElementById('reverse-button').onclick = function () {
 };
 document.getElementById('draw-button').onclick = function () {
 	deck.draw(1);
-};/Users/joelperry/Downloads/deck-of-cards-master/js/Deck.js
+};
